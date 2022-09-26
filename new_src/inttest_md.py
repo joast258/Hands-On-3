ac4ee5263cfb5d3f6aa7ad76410452fe3412167c
Joakim <joast258@student.liu.se>
2022-09-26 14:45:35 +0200

    added inittest

---
+++ new_src/inttest_md.py
@@ -0,0 +1,33 @@
+import hdmolecule
+import sys, unittest
+
+hdmolecule.run_md()
+
+class MdTests(unittest.TestCase):
+    def test_calcenergy(self):
+        from ase.lattice.cubic import FaceCenteredCubic
+        from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
+        from ase import units
+
+        from asap3 import EMT
+        size = 10
+        
+        atoms = FaceCenteredCubic(directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+                          symbol="Cu",
+                          size=(size, size, size),
+                          pbc=True)
+                          
+        atoms.calc = EMT()
+        
+        epot, ekin = hdmolecule.calcenergy(atoms)
+        
+        if(ekin<0):
+            self.assertTrue(false)
+        else:
+            self.assertTrue(True)
+            
+if __name__ == "__main__":
+    tests = [unittest.TestLoader().loadTestsFromTestCase(MdTests)]
+    testsuite = unittest.TestSuite(tests)
+    result = unittest.TextTestRunner(verbosity=0).run(testsuite)
+    sys.exit(not result.wasSuccessful())
